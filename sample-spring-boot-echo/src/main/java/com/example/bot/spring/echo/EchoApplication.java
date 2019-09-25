da7a67199aab2d9e03bdf3ea6199474668b145c3
HY-AndyPeng <47019223+HY-AndyPeng@users.noreply.github.com>
2019-09-25 17:40:25 +0800

    test

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -16,27 +16,16 @@
 
 package com.example.bot.spring.echo;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.linecorp.bot.model.ReplyMessage;
-import com.linecorp.bot.model.action.MessageAction;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.LocationMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.LocationMessage;
 import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TemplateMessage;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.message.template.ConfirmTemplate;
-import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
-import lombok.NonNull;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -49,25 +38,9 @@ public class EchoApplication {
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
         final String originalMessageText = event.getMessage().getText();
-        final String replyToken = event.getReplyToken();
-        
-        if("@ID".equals(originalMessageText)) {
-        	return new TextMessage(event.getSource().getUserId());
-        }
-        
-        if("@C".equals(originalMessageText)) {
-        	ConfirmTemplate confirmTemplate = new ConfirmTemplate(
-                    "Do it?",
-                    new MessageAction("Yes", "Yes!"),
-                    new MessageAction("No", "No!")
-            );
-            TemplateMessage templateMessage = new TemplateMessage("Confirm alt text", confirmTemplate);
-            return templateMessage;
-        }
-        
         return new TextMessage(originalMessageText);
     }
-    
+
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
