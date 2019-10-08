36bbdf78956e4d6c76b9b2bf3c20f9beaa84d2c9
HY-AndyPeng <47019223+HY-AndyPeng@users.noreply.github.com>
2019-10-08 13:08:12 +0800

    Test

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -16,7 +16,11 @@
 
 package com.example.bot.spring.echo;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
@@ -25,6 +29,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.fet.crm.nspMicro.util.bean.HttpResult;
 import com.fet.crm.nspMicro.util.bean.Records;
 import com.fet.crm.nspMicro.util.bean.Test;
+import com.fet.crm.nspMicro.util.bean.Time;
+import com.fet.crm.nspMicro.util.bean.WeatherElement;
 import com.linecorp.bot.model.action.DatetimePickerAction;
 import com.linecorp.bot.model.action.MessageAction;
 import com.linecorp.bot.model.event.Event;
@@ -51,7 +57,6 @@ public class EchoApplication {
         Message result = null;
         final String originalMessageText = event.getMessage().getText();
         final String token = event.getReplyToken();
-        Map<String, String> params = new HashMap<String, String>();
         
         switch(originalMessageText) {
         	case "87":
@@ -71,26 +76,84 @@ public class EchoApplication {
                 );
         		result = new TemplateMessage("Confirm alt text", confirmTemplate);
         		break;
-        	case "@test":	
-				try {
-					params.put("Authorization", "CWB-F17B9FA6-AE76-4DC5-BF8E-3D3E2EC19F63");
-					params.put("sort", "startTime");
-					params.put("locationName", "臺北市");
-					HttpResult httpResult = HttpUtil.get(
-							"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001", params);
-					String respon = httpResult.getResult();
-					Test test = JsonUtil.stringToObject(respon, Test.class);
-					
-					result = new TextMessage(test.getRecords().getDatasetDescription());
-				} catch (Exception e) {
-					result = new TextMessage("發根問題" + e.getMessage());
-				}
 			break;
         	default: 
-        		result = new TextMessage(originalMessageText);
+        		StringBuffer sb = new StringBuffer();
+        		
+        		if(true) {
+        			Test test = getWeather("臺北市");
+        			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        			sb.append("臺北市目前").append("\n");
+        			if(test.getRecords() != null) {
+        				Records records = test.getRecords();
+        				List<WeatherElement> weatherElements = records.getWeatherElement();
+        				
+        				for(WeatherElement weatherElement: weatherElements) {
+        					switch(weatherElement.getElementName()) {
+        						//天氣
+        						case "Wx":
+        				
+        							List<Time> times = weatherElement.getTime();
+        							Calendar nowCalendar = Calendar.getInstance();
+        							Calendar startCalendar = Calendar.getInstance();
+    								Calendar endCalendar = Calendar.getInstance();
+        							for(Time time : times) {
+        								Date startDate = sdf.parse(time.getStartTime());
+        								Date endDate = sdf.parse(time.getEndTime());
+        								startCalendar.setTime(startDate);
+        								endCalendar.setTime(endDate);
+        								
+        								if(nowCalendar.after(startCalendar) && nowCalendar.before(endCalendar)) {
+        									sb.append("天氣:").append(time.getParameter().get("parameterName"));
+        								}
+        							}
+        							
+        							break;
+        						//降雨機率
+        						case "PoP":	
+        							break;
+        						//最低溫度
+        						case "MinT":
+        							break;
+        						//最高溫度
+        						case "MaxT":
+        							break;
+        						//感覺
+        						case "CI":
+        							break;
+        					}
+        					
+        				}
+
+        			}
+        			
+        		}
+        		
+        		
+        		result = new TextMessage(sb.toString());
         }
         
         return result;
+    }
+    
+    @SuppressWarnings("unused")
+	private boolean hasKeyWord(String keyWord) {
+    	
+    	return true;
+    }
+    
+    private Test getWeather(String locationName) throws Exception {
+    	Test result = null;
+    	Map<String, String> params = new HashMap<String, String>();
+    	params.put("Authorization", "CWB-F17B9FA6-AE76-4DC5-BF8E-3D3E2EC19F63");
+		params.put("sort", "startTime");
+		params.put("locationName", "臺北市");
+		HttpResult httpResult = HttpUtil.get(
+				"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001", params);
+		String respon = httpResult.getResult();
+		result = JsonUtil.stringToObject(respon, Test.class);
+		
+		return result;
     }
     
     @EventMapping
